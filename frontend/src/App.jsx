fadd7eef87346a9589dc20522405891ae3c82189
schwamic <schwarz.michael@posteo.net>
2024-11-02 00:07:42 +0100

    feat(frontend): add basic websocket connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import './App.css'
 
+const WEB_SOCKET_URL = "ws://127.0.0.1:8000/ws/api/v1/conversations";
+const ID = "1a99a183-808c-4918-855a-93baff9b371b"
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [socketUrl, setSocketUrl] = useState(`${WEB_SOCKET_URL}/${ID}`);
+  const [messageHistory, setMessageHistory] = useState([]);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(`${WEB_SOCKET_URL}/${ID}`);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage]);
+
+
+  const handleClickSendMessage = useCallback(() => {
+    var message = {
+      type: "message",
+      data: "HELLO"
+    }
+    console.log("sendMessage", message)
+    sendMessage(JSON.stringify(message))
+
+  }, [sendMessage]);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
 
   return (
     <>
+      <h1>Cleon GPT</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={handleClickSendMessage}
+          disabled={readyState !== ReadyState.OPEN}
+        >
+          Click Me to send HELLO
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>The WebSocket is currently {connectionStatus}</div>
+      {lastMessage ? <div>Last message: {lastMessage.data}</div> : null}
+      <div>
+        <ul>
+          {messageHistory.map((message, idx) => (
+            <span key={idx}>{message ? message.data : null}</span>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
